e2b4ab70ad6c8fc7ba935ebf67a2db6369badcea
Yousef Roshandel <yousef.r1999@gmail.com>
2024-07-31 19:08:34 +0330

    Change the sacle to 1.05

--- components/Buttons/TransparentArrowBtn.vue
+++ components/Buttons/TransparentArrowBtn.vue
@@ -1,5 +1,5 @@
 <template>
-	<NuxtLink to="/products" class="button max-h-20 flex flex-wrap items-center justify-start space-x-2 hover:scale-110">
+	<NuxtLink to="/products" class="button max-h-20 flex flex-wrap items-center justify-start space-x-2 hover:scale-105">
 		<span class="uppercase whitespace-nowrap text-base font-bold tracking-wider">DISCOVER ALL PROJECTS</span>
 		<img class="h-10" src="/assets/icons/ArrowToCircleIcon.svg" alt="Arrow Icon" />
 	</NuxtLink>
