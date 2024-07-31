186687b0c8548442bcdfbd6f65efaa1f82673c7d
Yousef Roshandel <yousef.r1999@gmail.com>
2024-07-31 16:53:51 +0330

    implement Home page - Section 4 (Services)

--- pages/index.vue
+++ pages/index.vue
@@ -35,6 +35,53 @@
 				<AnimatedEclipse class="rotate-12" />
 			</div>
 		</section>
+		<section>
+			<div class="relative px-12 sm:px-16 lg:px-20 h-screen flex items-center justify-center">
+				<div class="text-left relative z-10">
+					<TitlesShadowTitle text="services" />
+					<h2 class="font-bold text-4xl tracking-wide uppercase">we provide</h2>
+					<div class="cards-container mt-11 hidden 2xl:grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-2.5">
+						<CardsServiceCard
+							title="Computer Aided design"
+							icon="iconoir:design-nib-solid"
+							description="Production of CAD modelling, simulations and rendering for mechanical and civil applications."
+							image="card1"
+						/>
+						<CardsServiceCard
+							title="Traffic Modelling & Simulation"
+							icon="lucide:traffic-cone"
+							description="Development of traffic models for highway assessments and traffic management scheme."
+							image="card2"
+						/>
+						<CardsServiceCard
+							title="Highway assessment trials"
+							icon="mdi:highway"
+							description="Assessments into new and innovative methodologies, technologies and systems and reporting of outcomes."
+							image="card3"
+						/>
+						<CardsServiceCard
+							title="Innovation management"
+							icon="teenyicons:bulb-on-outline"
+							description="Helping you to build environments where innovation can thrive and deliver on an organisation’s strategic objectives."
+							image="card4"
+						/>
+						<CardsServiceCard
+							title="Funding & Bidding"
+							icon="icon-park-outline:funds"
+							description="Supporting you to develop funding applications for UK and European funding. From first concepts to submission and project management."
+							image="card5"
+						/>
+						<CardsServiceCard
+							title="Development & Management"
+							icon="material-symbols:developer-mode-tv-outline"
+							description="Development, testing and deployment of software systems including analytics, web and applications."
+							image="card6"
+						/>
+					</div>
+				</div>
+				<AnimatedEclipse class="rotate-45" />
+			</div>
+		</section>
 		<AnimatedScrollIcon class="scrollIcon" />
 	</div>
 </template>
