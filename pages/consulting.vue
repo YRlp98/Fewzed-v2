d85f50d2f594e80f3fa9dc1c1e02e68c061fa2d2
Yousef Roshandel <yousef.r1999@gmail.com>
2024-08-07 17:17:11 +0330

    Add Fade in/out effect for the text

--- pages/consulting.vue
+++ pages/consulting.vue
@@ -1,37 +1,39 @@
 <template>
-	<div class="relative px-12 sm:px-16 lg:px-20 h-screen flex items-center justify-center overflow-y-auto">
+	<div class="relative px-12 sm:px-16 lg:px-20 h-screen flex items-center justify-center">
 		<div class="flex flex-wrap items-start gap-20 md:pt-0 pb-20 md:pb-0">
 			<!-- Left side -->
 			<div class="text-left relative min-w-[250px] max-w-[50rem] flex-1">
-				<div class="relative space-y-8 h-[500px] mt-12">
+				<div class="relative space-y-8 h-[300px] lg:mt-40">
 					<TitlesShadowTitle text="consulting" />
 
 					<!-- Scrolling Content -->
-					<div class="max-w-[800px]">
-						<p class="text-2xl tracking-wide leading-10 pb-12">
+					<div class="max-w-[800px] text-2xl tracking-wide leading-10 space-y-20 fade">
+						<p class="">
 							We’re here to support your business and projects with expert knowledge and experience from the Highways,
-							Construction, Data Collection and Sampling industries. 
+							Construction, Data Collection, and Sampling industries.
+						</p>
 
-							<br class="mt-16" />
+						<p>
 							Our small team of experts have demonstrated their capabilities over many years as project managers,
-							technical experts, innovation and technology strategists and trial and experiment managers
+							technical experts, innovation and technology strategists, and trial and experiment managers. We also have
+							access to a wide network of specialists to supplement our capabilities.
+						</p>
 
-							<br class="mt-16" />
-							We also have access to a wide network of specialists to supplement our capabilities. Our background is big
-							consultancy and technology development together with a wide knowledge of highways operations.
+						<p>
+							Our background is big consultancy and technology development together with a wide knowledge of highways
+							operations.
+						</p>
 
-							<br class="mt-16" />
+						<p class="pb-60">
 							We typically work with a blend of in person and remote delivery and even take on secondment opportunities.
-
-							<br class="mt-16" />
-							Get in touch with us  today to see how we can assist you with your specialist projects and applications.
+							Get in touch with us today to see how we can assist you with your specialist projects and applications.
 						</p>
 					</div>
 				</div>
 			</div>
 
 			<!-- Right side -->
-			<div class="relative flex-1 mt-20">
+			<div class="relative flex-1 mt-20 hidden md:block">
 				<canvas ref="canvas" class="m-auto" />
 			</div>
 		</div>
@@ -43,6 +45,10 @@ import { ref, reactive, onMounted } from "vue";
 import { Application } from "@splinetool/runtime";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+
+// definePageMeta({
+// 	layout: "custom",
+// });
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -80,5 +86,16 @@ onMounted(async () => {
 <style scoped>
 canvas {
 	pointer-events: none;
+}
+
+.fade {
+	background-image: linear-gradient(transparent 10%, #fff 30%, #fff 70%, transparent 90%);
+	background-clip: text;
+	-webkit-background-clip: text;
+	background-attachment: fixed;
+}
+
+.fade > * {
+	color: transparent;
 }
 </style>
