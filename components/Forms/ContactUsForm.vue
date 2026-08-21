d83412a691c9ab360923d7421964a4db47eee4af
Yousef <yousef.r1999@gmail.com>
2026-08-21 14:02:12 +0330

    update the contact form style

--- components/Forms/ContactUsForm.vue
+++ components/Forms/ContactUsForm.vue
@@ -5,26 +5,26 @@
 			<div class="flex flex-col items-start">
 				<label for="firstName" class="mb-1 ml-3 text-gray-4">First Name</label>
 				<input type="text" id="firstName" name="First name" placeholder="First Name" required
-					class="w-full bg-gray-1 rounded-lg placeholder-gray-4 text-gray-5 p-2 border-none outline-none" />
+					class="w-full rounded-[10px] border border-gray-2 bg-gray-1/20 p-2 text-gray-5 placeholder:text-gray-4/70 outline-none transition-colors duration-200 hover:border-purple-400/60 focus:border-purple-500 focus:ring-2 focus:ring-purple-500/30" />
 			</div>
 
 			<div class="flex flex-col items-start">
 				<label for="lastName" class="mb-1 ml-3 text-gray-4">Last Name</label>
 				<input type="text" id="lastName" name="Last name" placeholder="Last Name" required
-					class="w-full bg-gray-1 rounded-lg placeholder-gray-4 text-gray-5 p-2 border-none outline-none" />
+					class="w-full rounded-[10px] border border-gray-2 bg-gray-1/20 p-2 text-gray-5 placeholder:text-gray-4/70 outline-none transition-colors duration-200 hover:border-purple-400/60 focus:border-purple-500 focus:ring-2 focus:ring-purple-500/30" />
 			</div>
 		</div>
 
 		<div class="flex flex-col items-start">
 			<label for="email" class="mb-1 ml-3 text-gray-4">Email</label>
 			<input type="text" id="email" name="Email" placeholder="Email" required
-				class="w-full bg-gray-1 rounded-lg placeholder-gray-4 text-gray-5 p-2 border-none outline-none" />
+				class="w-full rounded-[10px] border border-gray-2 bg-gray-1/20 p-2 text-gray-5 placeholder:text-gray-4/70 outline-none transition-colors duration-200 hover:border-purple-400/60 focus:border-purple-500 focus:ring-2 focus:ring-purple-500/30" />
 		</div>
 
 		<div class="flex flex-col items-start">
 			<label for="message" class="mb-1 ml-3 text-gray-4">Message</label>
 			<textarea id="message" name="Message" placeholder="Message"
-				class="w-full h-32 bg-gray-1 rounded-lg placeholder-gray-4 text-gray-5 p-2 border-none outline-none resize-none"></textarea>
+				class="h-32 w-full resize-none rounded-[10px] border border-gray-2 bg-gray-1/20 p-2 text-gray-5 placeholder:text-gray-4/70 outline-none transition-colors duration-200 hover:border-purple-400/60 focus:border-purple-500 focus:ring-2 focus:ring-purple-500/30"></textarea>
 		</div>
 
 		<Button type="submit"
