8f3b4322fc688519da5865e29567073fbe44b37a
Yousef <yousef.r1999@gmail.com>
2026-09-06 18:51:57 +0330

    update the products images

--- components/Cards/ProjectCard.vue
+++ components/Cards/ProjectCard.vue
@@ -19,8 +19,8 @@
 
 <script setup lang="ts">
 import { computed } from "vue";
-import sytemonitor from "~/assets/images/sytemonitor.webp";
-import flohtex from "~/assets/images/police-image.jpg";
+import sytemonitor from "~/assets/images/road-image.webp";
+import flohtex from "~/assets/images/police-image.webp";
 import surveys365 from "~/assets/images/365surveys.webp";
 
 const images = {
